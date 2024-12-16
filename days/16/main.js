84571ef3222b27d1298b5b8416e91721d7484490
Matt Watson <mwatson@renci.org>
2024-12-16 16:00:02 -0500

    wip

--- days/16/main.js
+++ days/16/main.js
@@ -6,12 +6,10 @@ function hash(i, j) { return `${ i },${ j }`; }
 function unhash(str) { return str.split(',').map(Number); }
 
 function parse(lines) {
-  const height = lines.length;
-  const width = lines[0].length;
-  const hashes = [];
+  const height = lines.length,
+        width = lines[0].length;
   return lines.reduce((acc, line, y) => {
     for (let x = 0; x < width; x += 1) {
-      hashes.push(hash(x, y));
       if (line[x] in acc) {
         acc[line[x]].push({ x, y });
       } else {
@@ -19,72 +17,67 @@ function parse(lines) {
       }
     }
     return acc;
-  }, {
-    '#': [],
-    width,
-    height,
-    map: lines,
-    hashes,
-  });
+  }, { width, height, map: lines });
 };
 
-function walkMaze(maze) {
-  const dirs = [
-    { x: 0, y: 1 },
-    { x: 1, y: 0 },
-    { x: 0, y: -1 },
-    { x: -1, y: 0 },
-  ];
-  const paths = [];
+const dirs = [{ x: 0, y: -1 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 0 }];
 
-  function step({ x, y }, path = []) {
-    path.push(hash(x, y));
+function cost(path) {
+  // probably could calculate this along the walk
+  let turnCount = 0;
+  let lastDir = null;
+  for (let i = 1; i < path.length - 1; i += 1) {
+    const { x, y } = path[i];
+    const { x: prevX, y: prevY } = path[i - 1];
+    const thisDir = hash(x - prevX, y - prevY);
+    if (thisDir !== lastDir) { turnCount += 1; }
+    lastDir = thisDir;
+  }
+  return path.length - 1 + 1000 * turnCount;
+}
 
-    function validStep({ x, y }) {
-      return (maze.map[y][x] === '.' || maze.map[y][x] === 'E') 
-        && !path.includes(hash(x, y))
-    }
+function findMinPath(maze) {
+  let minPath = null;
+  let minCost = Infinity;
 
+  let maxL = 0;
+  function step({ x, y }, path = [], visited = new Set()) {
+    path.push({ x, y });
+    const hsh = hash(x, y);
+    visited.add(hsh);
+    maxL = Math.max(maxL, path.length);
+    console.log(' '.repeat(path.length), path.length, maxL);
+    const valid = ({ x, y }) => 
+        (0 <= x && x < maze.width) && (0 <= y && y < maze.height) // on map
+        && (maze.map[y][x] === '.' || maze.map[y][x] === 'E')     // valid char
+        && !visited.has(hash(x, y));                              // unvisited
+    // at end?
     if (maze.map[y][x] === 'E') {
-      paths.push([...path]);
-      return;
-    }
-
-    for (let d of dirs) {
-      const next = { x: x + d.x, y: y + d.y };
-      if (validStep(next)) {
-        step(next, path);
-      } else if (maze.map[y][x] === '#') {
-        paths.push([...path]);
-        path.pop();
+      const pathCost = cost(path);
+      if (pathCost < minCost) {
+        minCost = pathCost;
+        minPath = [...path];
       }
     }
+    // look around, take steps as able
+    for (const dir of dirs) {
+      const next = { x: x + dir.x, y: y + dir.y };
+      if (valid(next)) { step(next, path, visited); }
+    }
+    path.pop();
+    visited.delete(hsh);
   }
-
-  const start = maze.S[0];
+  const start = maze.S?.[0];
+  if (!start) throw new Error('maze does not have a start "S".');
   step(start);
-
-  return paths[0];
+  return minPath;
 }
 
 export const part1 = function(input) {
   const maze = parse(input);
-  const path = walkMaze(maze);
-  let turnCount = 0;
-  let lastDir = null;
+  const path = findMinPath(maze);
   console.log(path);
-  console.log(new Set([...path]));
-  for (let i = 0; i < path.length - 2; i += 1) {
-    const [x, y] = unhash(path[i]);
-    const thisDir = hash(x - unhash(path[i-1])[0], y - unhash(path[i-1])[1]);
-    console.log(path[i], {thisDir, lastDir})
-    if (lastDir && thisDir !== lastDir) {
-      turnCount += 1;
-    }
-    lastDir = hash(x, y);
-  }
-  console.log({ l: path.length, turnCount })
-  return path.length + 1000 * turnCount;
+  return cost(path);
 };
 
 export const part2 = function(input) {
