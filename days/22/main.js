c73d5b3f18c04bf6b9cdf0615919f02551b8910e
Matt Watson <mwatson@renci.org>
2024-12-22 15:54:21 -0500

    switch to generator
    switch to generator

--- days/22/main.js
+++ days/22/main.js
@@ -1,35 +1,47 @@
 /*
   --- Day 22: Monkey Market ---
+
+  part1: 565.747ms
+  part2: 
+  Solutions: { part1: 15303617151n, part2:  }
+
 */
 
 
-function evolve(secret) {
-  const mult64 = n => n << 6n;
-  const div32 = n => n >> 5n;
-  const mult2048 = n => n << 11n;
-  const mix = (n1, n2) => n1 ^ n2;
-  const prune = n => (n + (1n << 24n)) & ((1n << 24n) - 1n);
+const mult64 = n => n << 6n;
+const div32 = n => n >> 5n;
+const mult2048 = n => n << 11n;
+const mix = (n1, n2) => n1 ^ n2;
+const prune = n => (n + (1n << 24n)) & ((1n << 24n) - 1n);
 
-  let result;
-  result = prune(mix(mult64(secret), secret));
-  result = prune(mix(div32(result), result));
-  result = prune(mix(mult2048(result), result));
-  return result;
+function* evolve(init) {
+  let result = init;
+  while (true) {
+    yield result;
+    result = prune(mix(mult64(result), result));
+    result = prune(mix(div32(result), result));
+    result = prune(mix(mult2048(result), result));
+  }
 }
 
 export const part1 = function(input) {
-
   const initials = input.map(BigInt);
   return initials.reduce((acc, initial) => {
-    let result = initial;
+    const secrets = evolve(initial);
+    let secret = secrets.next();
     for (let i = 0; i < 2000; i++) {
-      result = evolve(result);
+      secret = secrets.next().value;
     }
-    acc.push([initial, result])
+    acc += secret;
     return acc;
-  }, []).reduce((sum, [init, n]) => sum + n, 0n);
+  }, 0n);
 };
 
 export const part2 = function(input) {
-  return null;
+  const initials = input.map(BigInt);
+  return initials.reduce((acc, initial) => {
+    const secrets = [...evolve(initial];
+    console.log(secrets);
+    console.log(secrets.length);
+  }, 0n);
 };
